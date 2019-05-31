93a5201709ef2094c3e7b6d3835f21ccf96b95c5
huutai <truongtai137@gmail.com>
2019-06-01 00:04:36 +0700

    initial commit

---
+++ hosts.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+rm -rf parts
+mkdir parts
+
+while read url; do
+	filename=`grep -oP '(?<=://).*' <<< "$url" | tr '/' '_'`
+	curl "$url" -o "parts/$filename"
+done <sources.txt
+
+cat `find parts -type f` user.hosts \
+	| grep -oP "^\d+\.\d+\.\d+\.\d+.*$" \
+	| cut -d'#' -f1 \
+	| awk '{$1=$1};1' \
+	| awk '!a[$0]++' \
+	| egrep -v "^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+ $(printf '(%s)' $(printf %s "$(awk NF whitelist.txt)" | tr '\n' '|'))$" \
+	>hosts
