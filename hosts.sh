da077c78ad5a1e74c41e2da27aad0b776b39f195
huutai <truongtai137@gmail.com>
2019-06-01 16:11:35 +0700

    stupid me forgetting that IPv6 exists

--- hosts.sh
+++ hosts.sh
@@ -8,10 +8,10 @@ while read url; do
 	curl "$url" -o "parts/$filename"
 done <sources.txt
 
-cat `find parts -type f` user.hosts \
-	| grep -oP "^\d+\.\d+\.\d+\.\d+.*$" \
-	| cut -d'#' -f1 \
-	| awk '{$1=$1};1' \
-	| awk '!a[$0]++' \
-	| egrep -v "^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+ $(printf '(%s)' $(printf %s "$(awk NF whitelist.txt | cut -d'#' -f1 | awk '{$1=$1};1')" | tr '\n' '|'))$" \
+cat `find parts -type f` user.hosts |
+	grep -oP "^[0-9a-f:.]+[ \t]+.*$" | # filter hosts entries
+	cut -d'#' -f1 |	awk '{$1=$1};1' | # trailing comments
+	awk '!a[$0]++' | # remove duplicate entries
+	# apply whitelist
+	egrep -v "^(0\.0\.0\.0|127\.0\.0\.1)[ \t]+$(printf '(%s)' $(printf %s "$(awk NF whitelist.txt | cut -d'#' -f1 | awk '{$1=$1};1')" | tr '\n' '|'))$" \
 	>hosts
